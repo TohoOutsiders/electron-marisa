12dfc535e4d239c9e9aa4b7b7b8ecfd9f3defd19
gutrse3321 <gutrse3321@live.com>
2018-06-17 16:31:23 +0800

    添加分词字典，优化学习与回复功能

--- src/renderer/views/Home/chatroom.vue
+++ src/renderer/views/Home/chatroom.vue
@@ -4,18 +4,13 @@
       <div class="talk-panel">
         <span>うるさい! うるさい.. うるさい...</span>
         <div ref="talk_place" class="talk-place">
-          <div class="talk_entry"
-              v-for="item in talk_list"
-              :class="{'you_color': item.name == 'You'}"
-          >
-            <span class="talk_item" v-text="item.name" :class="{'you_color': item.name == 'You'}"></span>&nbsp;:&nbsp;<span class="talk_item" v-text="item.content" :class="{'you_color': item.name == 'You'}"></span>
+          <div class="talk_entry" v-for="item in talk_list" :class="{'you_color': item.name == 'You'}">
+            <span class="talk_item" v-text="item.name" :class="{'you_color': item.name == 'You'}"></span>&nbsp;:&nbsp;
+            <span class="talk_item" v-html="item.content" :class="{'you_color': item.name == 'You'}"></span>
           </div>
         </div>
         <div class="speak">
-          <input @keydown="sendMessage($event)"
-                ref="you"
-                v-focus="true" type="text" name="you"
-          />
+          <input @keydown="sendMessage($event)" ref="you" v-focus="true" type="text" name="you" />
           <input @click="sendMessage($event)" ref="submit" type="submit" value="发送" />
         </div>
       </div>
@@ -29,13 +24,16 @@
             <span class="marisa-cmd">teach</span>&nbsp;进入内容教学模式
           </span>
           <span class="system-cmd cmd-collect">
-            <del><span class="marisa-cmd">forget</span>&nbsp;忘记最后所说的内容</del>
+            <del>
+              <span class="marisa-cmd">forget</span>&nbsp;忘记最后所说的内容</del>
           </span>
           <span class="system-cmd cmd-collect">
-            <del><span class="marisa-cmd">application</span>&nbsp;管理外部应用接口</del>
+            <del>
+              <span class="marisa-cmd">application</span>&nbsp;管理外部应用接口</del>
           </span>
           <span class="system-cmd cmd-collect">
-            <del><span class="marisa-cmd">status</span>&nbsp;查看目前知识所掌握情况</del>
+            <del>
+              <span class="marisa-cmd">status</span>&nbsp;查看目前知识所掌握情况</del>
           </span>
           另外你也可以通过输入
           <del style="font-weight:bold;">hint</del> 查看其他人自定义的内容提示或小小线索<br><br> 魔理沙无条件的相信你..她把你交给她的所有知识视作珍宝并会很认真的将其牢牢记住..不要让她学坏哦!
@@ -79,7 +77,7 @@ export default {
         case 'teach':
           this.talk_list.push({
             name: MARISA,
-            content: '好哒，你要教我什么呢？格式：You的消息`白絲魔理沙的回复'
+            content: '<br />好的，你要教我什么呢？<br/ >格式：You的消息`白絲魔理沙的回复'
           })
           this.cmd_flag = 1
           break
@@ -88,14 +86,29 @@ export default {
     },
     _marisaReply (_content) {
       let memorise = this.$db.get('memorise').value()
+      let _pplContent = this.$dictionary.doSegment(_content, {simple: true})
       let answer = ''
+      let keywords = []
+
+      // 处理获取的_content到数据库去遍历查询
+      // 大于80%就回复对应回答
       for (let i = 0; i < memorise.length; i++) {
-        memorise[i].keyword.forEach(item => {
-          if (item === _content) {
+        let ratio = 0
+        keywords = memorise[i].keyword
+        for (let j = 0; j < keywords.length; j++) {
+          _pplContent.forEach(ppl => {
+            if (keywords[j] === ppl) {
+              ratio++
+            }
+          })
+          console.log('ratio:' + ratio)
+          if ((ratio / keywords.length) > 0.8) {
             answer = memorise[i].answer
+            break
           }
-        })
+        }
       }
+
       if (answer !== '') {
         this.talk_list.push({
           name: MARISA,
@@ -106,20 +119,55 @@ export default {
           name: MARISA,
           content: '唔嗯...不懂你在说什么呢...'
         })
+        console.log(this.$dictionary.doSegment(_content, {
+          simple: true
+        }))
       }
     },
     _teachMarisa (_content) {
+      // 将you`marisa格式转换为[you,marisa]数组
       let str = _content.split('`')
-      let memorey = {
-        keyword: [
-          str[0]
-        ],
-        answer: str[1]
+      // 将you的句子分词分一个数组
+      let toPpl = this.$dictionary.doSegment(str[0], {simple: true})
+      // 获取数据库所有的记忆
+      let memorise = this.$db.get('memorise').value()
+      // 遍历数据库查询匹配的回答
+      let keywords = []
+      let memorey = {}
+
+      for (let i = 0; i < memorise.length; i++) {
+        let ratio = 0
+        keywords = memorise[i].keyword
+        for (let j = 0; j < keywords.length; j++) {
+          toPpl.forEach(ppl => {
+            if (keywords[j] === ppl) {
+              ratio++
+            }
+          })
+          if ((ratio / keywords.length) > 0.5) {
+            keywords.concat(toPpl)
+            // 去除重复的关键词或字
+            keywords = Array.from(new Set(keywords.filter((x, i, self) => self.indexOf(x) === i)))
+            memorey = {
+              keyword: keywords,
+              answer: str[1]
+            }
+            break
+          } else {
+            memorey = {
+              keyword: toPpl,
+              answer: str[1]
+            }
+            break
+          }
+        }
       }
+      // 插入记忆
       this.$db.get('memorise').push(memorey).write()
+      // 学习模式解除，默认回复，切换到聊天模式
       this.talk_list.push({
         name: MARISA,
-        content: `ok，当你说"${str[0]}"的时候，嗯...好！老子就说"${str[1]}"DA☆ZE`
+        content: '行，我知道了'
       })
       this.cmd_flag = 0
     },
@@ -136,120 +184,156 @@ export default {
   created () {
     let _startTalk = {
       name: MARISA,
-      content: '白絲魔理沙 Type 0.001,还在继续升级DA☆ZE！'
+      content: '白絲魔理沙 Type 0.003,还在继续升级DA☆ZE！'
     }
     this.talk_list.push(_startTalk)
   }
 }
 </script>
 <style lang="stylus" scoped>
-@import "~@/assets/stylus/variable"
+@import '~@/assets/stylus/variable';
 
-.chatroom
-  width: 712px
-  height: 512px
-  background: $box-color
-  border: 1px solid $border-color
-.container
-  display: flex
-  justify-content: space-between
-  margin: 0 auto
-  margin-top: 6px
-  width: 700px
-  height: 502px
-.talk-panel
-  width: 470px
-  height: 500px
-  background: $container-color
-  border: 1px solid $border-color
-  span
-    display: block
-    width: 100%
-    height: 40px
-    background: $title-color
-    border-bottom: 1px solid $border-color
-    font-size: 14px
-    line-height: 40px
-    text-indent: 9px
-    color: $order-blue
-  .talk-place
-    width: clac(100% - 10px)
-    height: 420px
-    padding-left: 10px
-    overflow-y: scroll
-    .talk_entry
-      width: 100%
-      font-size: 14px
-      margin-top: 12px
-      word-break: break-all
-      line-height: 25px
-      .talk_item
-        display: inline
-        background: transparent
-        border: none
-        line-height: 0
-  .speak
-    width: 100%
-    height: 39px
-    input
-      background: none
-      outline: 0
-      border: none
-    & input[name='you']
-      position: relative
-      top: 10px
-      left: 5px
-      width: 382px
-      height: 24px
-      padding-left: 5px
-      background: rgba(255, 255, 255, .2)
-      border: 1px solid $border-color
-      transition: all .2s
-      &:hover,&:focus
-        background: rgba(255, 255, 255, .6)
-        border-radius: 5px
-    & input[type='submit']
-      position: relative
-      top: 9px
-      left: 15px
-      width: 48px
-      height: 22px
-      background: rgba(255, 255, 255, .2)
-      border: 1px solid $border-color
-      border-radius: 5px
-      font-size: 12px
-      transition: all .2s
-.profile
-  width: 220px
-  height: 502px
-  .avatar
-    width: 218px
-    height: 250px
-    border: 1px solid $border-color
-    background-image: url(./image/marisa.jpg)
-    background-size: 333px
-    background-repeat: no-repeat
-    background-position: 58% 2%
-    margin-bottom: 8px
-  .cmd
-    width: 202px
-    height: 232px
-    padding: 4px 8px
-    background: $container-color
-    border: 1px solid $border-color
-    color: $order-blue
-    font-size: 12px
-    .system-cmd
-      display: block
-      width: 100%
-      margin-bottom: 13px
-      margin-top: 13px
-    .cmd-collect
-      text-indent: 1em
-    .marisa-cmd
-      display: inline
-      font-weight: bold
+.chatroom {
+  width: 712px;
+  height: 512px;
+  background: $box-color;
+  border: 1px solid $border-color;
+}
 
-.you_color
-  color: #836FFF !important
+.container {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  margin-top: 6px;
+  width: 700px;
+  height: 502px;
+}
+
+.talk-panel {
+  width: 470px;
+  height: 500px;
+  background: $container-color;
+  border: 1px solid $border-color;
+
+  span {
+    display: block;
+    width: 100%;
+    height: 40px;
+    background: $title-color;
+    border-bottom: 1px solid $border-color;
+    font-size: 14px;
+    line-height: 40px;
+    text-indent: 9px;
+    color: $order-blue;
+  }
+
+  .talk-place {
+    width: clac(100% - 10px);
+    height: 420px;
+    padding-left: 10px;
+    overflow-y: scroll;
+
+    .talk_entry {
+      width: 100%;
+      font-size: 14px;
+      margin-top: 12px;
+      word-break: break-all;
+      line-height: 25px;
+
+      .talk_item {
+        display: inline;
+        background: transparent;
+        border: none;
+        line-height: 0;
+      }
+    }
+  }
+
+  .speak {
+    width: 100%;
+    height: 39px;
+
+    input {
+      background: none;
+      outline: 0;
+      border: none;
+    }
+
+    & input[name='you'] {
+      position: relative;
+      top: 10px;
+      left: 5px;
+      width: 382px;
+      height: 24px;
+      padding-left: 5px;
+      background: rgba(255, 255, 255, 0.2);
+      border: 1px solid $border-color;
+      transition: all 0.2s;
+
+      &:hover, &:focus {
+        background: rgba(255, 255, 255, 0.6);
+        border-radius: 5px;
+      }
+    }
+
+    & input[type='submit'] {
+      position: relative;
+      top: 9px;
+      left: 15px;
+      width: 48px;
+      height: 22px;
+      background: rgba(255, 255, 255, 0.2);
+      border: 1px solid $border-color;
+      border-radius: 5px;
+      font-size: 12px;
+      transition: all 0.2s;
+    }
+  }
+}
+
+.profile {
+  width: 220px;
+  height: 502px;
+
+  .avatar {
+    width: 218px;
+    height: 250px;
+    border: 1px solid $border-color;
+    background-image: url('./image/marisa.jpg');
+    background-size: 333px;
+    background-repeat: no-repeat;
+    background-position: 58% 2%;
+    margin-bottom: 8px;
+  }
+
+  .cmd {
+    width: 202px;
+    height: 232px;
+    padding: 4px 8px;
+    background: $container-color;
+    border: 1px solid $border-color;
+    color: $order-blue;
+    font-size: 12px;
+
+    .system-cmd {
+      display: block;
+      width: 100%;
+      margin-bottom: 13px;
+      margin-top: 13px;
+    }
+
+    .cmd-collect {
+      text-indent: 1em;
+    }
+
+    .marisa-cmd {
+      display: inline;
+      font-weight: bold;
+    }
+  }
+}
+
+.you_color {
+  color: #836FFF !important;
+}
 </style>
